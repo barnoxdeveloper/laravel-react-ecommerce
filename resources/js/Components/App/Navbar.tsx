42b4850c656accfff9e017d902ef956900a4d91d
barnoxdeveloper <barnoxdev@gmail.com>
2025-02-19 21:31:12 +0700

    Create and user new Navbar using daisyUI

--- resources/js/Components/App/Navbar.tsx
+++ resources/js/Components/App/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 function Navbar() {
+    const { auth } = usePage().props;
+    const { user } = auth;
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
@@ -9,7 +11,7 @@ function Navbar() {
                     LaraStore
                 </Link>
             </div>
-            <div className="flex-none">
+            <div className="flex-none gap-4">
                 <div className="dropdown dropdown-end">
                     <div
                         tabIndex={0}
@@ -51,42 +53,57 @@ function Navbar() {
                         </div>
                     </div>
                 </div>
-                <div className="dropdown dropdown-end">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar"
-                    >
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
+                {user && (
+                    <div className="dropdown dropdown-end">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost btn-circle avatar"
+                        >
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                />
+                            </div>
                         </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                        >
+                            <li>
+                                <Link
+                                    href={route("profile.edit")}
+                                    className="justify-between"
+                                >
+                                    Profile
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                    >
-                        <li>
-                            <Link
-                                href={route("profile.edit")}
-                                className="justify-between"
-                            >
-                                Profile
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href={route("logout")}
-                                method="post"
-                                as="button"
-                            >
-                                Logout
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                )}
+                {!user && (
+                    <>
+                        <Link href={route("login")} className={"btn"}>
+                            Login
+                        </Link>
+                        <Link
+                            href={route("register")}
+                            className={"btn btn-primary"}
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
             </div>
         </div>
     );
